1d955871237da7994bca627bb70eaa942bdc8721
kounet95 <85202452+kounet95@users.noreply.github.com>
2025-02-23 00:18:13 -0500

    ko

---
+++ programService/src/test/java/kounettechnologie/com/programservice/ProgramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kounettechnologie.com.programservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
